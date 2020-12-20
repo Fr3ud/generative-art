b7086497b28403d3d65a392ab5d86bd53f5aa727
Fr3ud <oo.pavelko@gmail.com>
2020-12-20 22:10:21 +0200

    feat: add noise, random seed, rotate and suffix

--- workshop/sketch.js
+++ workshop/sketch.js
@@ -3,8 +3,12 @@ const { lerp } = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
 const palettes = require('nice-color-palettes');
 
+random.setSeed(random.getRandomSeed());
+console.log(random.getSeed());
+
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  suffix: random.getSeed(),
+  dimensions: [ 2048, 2048 ],
 };
 
 const sketch = () => {
@@ -19,9 +23,11 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.2;
 
         points.push({
-          radius: Math.abs(random.gaussian() * 0.0042),
+          radius,
+          rotation: random.noise2D(u, v),
           position: [u, v],
           color: random.pick(palette),
         });
@@ -31,7 +37,7 @@ const sketch = () => {
     return points;
   };
 
-  random.setSeed(42);
+
   const points = createGrid().filter(() => random.value() > 0.5);
 
   return ({ context, width, height }) => {
@@ -43,6 +49,7 @@ const sketch = () => {
     points.forEach((data) => {
       const {
         position,
+        rotation,
         radius,
         color
       } = data;
@@ -52,10 +59,20 @@ const sketch = () => {
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
+
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Futura"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('=', 0, 0);
+
+      context.restore();
     });
   };
 };
@@ -64,6 +81,8 @@ canvasSketch(sketch, settings);
 
 /**
  * NOTES
+ *
+ * canvas-sketch sketch.js --open
  *
  * NOISE
  * const v = noise2D(x, y); // value is in -1..1 range
