9c8c0589af62c94f0d51e719898c7c52c2834191
Fr3ud <oo.pavelko@gmail.com>
2021-01-02 20:00:56 +0200

    feat: change form

--- workshop/shader.js
+++ workshop/shader.js
@@ -21,8 +21,12 @@ const frag = glsl(`
     vec3 colorA = vec3(1.0, 0.0, 0.0);
     vec3 colorB = vec3(0.0, 1.0, 0.0);
     
+    vec2 center = vUv - 0.5;
+    float dist = length(center);
+   
+    
     vec3 color = mix(colorA, colorB, vUv.x);
-    gl_FragColor = vec4(color, 1.0);
+    gl_FragColor = vec4(color, dist > 0.25 ? 0.0 : 1.0);
   }
 `);
 
